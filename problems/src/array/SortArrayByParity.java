35c68ba7e7e73492c60c6a50b521206b1de58023
Danica Nacionales <danicanacionales@gmail.com>
2019-03-24 23:10:49 +0800

    added solution
    march 24, 2019

---
+++ problems/src/array/SortArrayByParity.java
@@ -0,0 +1,34 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortArrayByParity {
+    public static void main(String[] args) {
+        int [] A = { 3, 1, 2, 4 };
+        A = new SortArrayByParity().sortArrayByParity(A);
+    }
+
+    public int[] sortArrayByParity(int[] A) {
+        List<Integer> odd = new ArrayList<>();
+        List<Integer> even = new ArrayList<>();
+
+        for(int i : A){
+            if((i % 2) == 0)
+                even.add(i);
+            else
+                odd.add(i);
+        }
+
+        for(int i = 0; i < even.size(); i++){
+            A[i] = even.get(i);
+        }
+
+        for(int i = even.size(); i < A.length; i++){
+            A[i] = odd.get(i - even.size());
+        }
+
+        return A;
+    }
+
+}
