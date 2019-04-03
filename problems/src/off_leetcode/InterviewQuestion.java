e136ed08fbbcdf8c04c83b63ea86f50e2e6072a7
danica nacionales <danicanacionales@gmail.com>
2019-04-03 23:32:43 +0800

    add topic
    april 3, 2019 - follow up content

---
+++ problems/src/off_leetcode/InterviewQuestion.java
@@ -0,0 +1,13 @@
+package off_leetcode;
+
+public class InterviewQuestion {
+
+    // 1. Factorial
+    // 2. Fibonacci
+    // 3. Series of Three
+    // 4. Max Profit
+    // 5. X Primes
+    // 6. Y Lowest Point
+    // 7. Aliens
+    // 8. Friends Console
+}
